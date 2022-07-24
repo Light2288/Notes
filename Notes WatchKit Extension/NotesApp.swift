1bf3f1f06dafb6a3a120dd6ab48fc28a3479a66f
Light2288 <davide.aliti@gmail.com>
2022-07-24 23:01:17 +0200

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Davide Aliti on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
